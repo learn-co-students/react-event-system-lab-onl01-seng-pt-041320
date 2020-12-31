3c5545f56360df8fd9f9facbadf0e4cfc638652b
Dice-K39 <daisukehashimoto39@gmail.com>
2020-12-30 19:14:27 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component
+{
+    focusEvent = event =>
+    {
+        console.log("Good!");
+    }
+
+    blurEvent = event =>
+    {
+        console.log("Hey! Eyes on me!");
+    }
+
+    render()
+    {
+        return (
+            <button onFocus={this.focusEvent} onBlur={this.blurEvent}>Push Me!</button>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component
+{
+    logInput = () =>
+    {
+        console.log("Entering password...");
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <input type="password" onKeyUp={this.logInput}/>
+            </div>
+        );
+    }
+}
+
+export default Keypad;
